955c03de5afd27e6e3509337d551813b50d99f58
Angela92806 <149626375+Angela92806@users.noreply.github.com>
2024-02-14 23:05:40 +0000

    Added Questions

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import './App.css'
 import data from "./sample_data.json";
+import Question from "./componets/Question.jsx";
+
 
 export default function App() {
   return (
     <div className="app">
       <h1 className="title">Trivia!</h1>
+      <Question Question={props.Question}/>
     </div>
   );
 }
---
+++ src/componets/Question.jsx
@@ -0,0 +1,7 @@
+export default function Question(props) {
+  return (
+    <div>
+    <p></p>
+    </div>
+  );}
+
